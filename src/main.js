02a39366d836ff4daf5820d749e71a8c7c53e6fe
Sushank Gurung <jagatgurung30@gmail.com>
2025-03-05 22:23:59 +0545

    drawing-board-complete

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
 const canvas = document.getElementById("canvas");
 
-canvas.style.width = "400px";
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight - 60;
+
+const c = canvas.getContext("2d");
+
+c.fillStyle = "red";
+c.fillRect(0, 0, 150, 75);
+c.fillRect(100, 100, 150, 75);
+c.fillRect(200, 200, 150, 75);
+c.fillRect(300, 300, 150, 75);
+c.fillRect(400, 400, 150, 75);
+
+document.addEventListener("mousemove", (e) => {
+  c.fillStyle;
+});
